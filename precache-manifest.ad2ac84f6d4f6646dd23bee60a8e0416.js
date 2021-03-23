c0a90acd047749ac779f88ddbe3d35572256eafa
emassaki <massakihirayama@gmail.com>
2021-03-22 22:33:19 -0300

    Updates

---
+++ precache-manifest.ad2ac84f6d4f6646dd23bee60a8e0416.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b60769e398506566867ba4e63cc6822",
+    "url": "/movie-library/index.html"
+  },
+  {
+    "revision": "3c13758455619b8a524e",
+    "url": "/movie-library/static/css/main.7c23fe44.chunk.css"
+  },
+  {
+    "revision": "1059ce9dd96dc5d5023f",
+    "url": "/movie-library/static/js/2.d9c6b01b.chunk.js"
+  },
+  {
+    "revision": "3c13758455619b8a524e",
+    "url": "/movie-library/static/js/main.78402435.chunk.js"
+  },
+  {
+    "revision": "f36309cfaf9b7d8c1e54",
+    "url": "/movie-library/static/js/runtime-main.03811eeb.js"
+  }
+]);
